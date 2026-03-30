c9d039d5c34e0476763f8e1aa1dd813101161868
Haseeb <haseeburrehman22346@gmail.com>
2026-03-31 01:02:44 +0500

    Ammad updat

--- src/components/Resul.tsx
+++ src/components/Resul.tsx
@@ -12,7 +12,7 @@ export const ResultCard = ({ isPassed, score, data }: { isPassed: boolean, score
                         <p className="text-sm sm:text-base md:text-lg lg:text-xl text-green-500">
                             Congratulations! You Passed! Great job!  hello HAseeb 🎉
                         </p>) : (<p className="text-sm sm:text-base md:text-lg lg:text-xl text-red-500">
-                            Sorry! You Failed. Better luck next time 😊
+                            Sorry! You Failed. Better luck next time Hello Ammad 😊
                         </p>
                     )}
                     <Link className="text-sm sm:text-base md:text-lg lg:text-xl font-[400] px-4 py-2 bg-[#dcdedc] border border-black rounded-md hover:bg-gray-400 transition-colors cursor-pointer w-[200px] text-center" href="/"> Go to Home </Link>
